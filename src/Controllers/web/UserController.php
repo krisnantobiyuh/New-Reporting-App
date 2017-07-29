67c8a6ca036bd6f4daaaaa9612f92c3be194b053
Imam Syafii <shafii@gmx.com>
2017-07-29 10:24:20 +0700

    add web view template

--- src/Controllers/web/UserController.php
+++ src/Controllers/web/UserController.php
@@ -26,6 +26,49 @@ class UserController extends BaseController
 // }
         var_dump($data->reporting->status);
     }
+
+    public function getLogin($request, $response)
+    {
+        return  $this->view->render($response, 'auth/login.twig');
+    }
+
+    public function login($request, $response)
+    {
+        // var_dump($request->getParams()['optlogin']);die();
+        $user = new \App\Models\Users\UserModel($this->db);
+        $group =  new \App\Models\UserGroupModel($this->db);
+        $guardian =  new \App\Models\GuardModel($this->db);
+
+        $login = $user->find('username', $request->getParam('username'));
+        $users = $guardian->findAllUser($login['id']);
+        $groups = $group->findAllGroup($login['id']);
+        // var_dump($login);die();
+
+        if (empty($login)) {
+            $this->flash->addMessage('warning', 'Username tidak terdaftar!');
+            return $response->withRedirect($this->router
+            ->pathFor('login'));
+        } else {
+            if (password_verify($request->getParam('password'),$login['password'])) {
+
+                $_SESSION['login'] = $login;
+
+                if ($_SESSION['login']['status'] == 2) {
+                    $_SESSION['user_group'] = $groups;
+
+                    $this->flash->addMessage('succes', 'Selamat datang, '. $login['name']);
+                    return $response->withRedirect($this->router->pathFor('home'));
+                } else {
+                    $this->flash->addMessage('warning', 'Anda bukan user');
+                    return $response->withRedirect($this->router->pathFor('login'));
+                }
+
+            } else {
+                $this->flash->addMessage('warning', 'Password salah!');
+                return $response->withRedirect($this->router->pathFor('login'));
+            }
+        }
+    }
 }
 
 
